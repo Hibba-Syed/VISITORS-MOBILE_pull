b6b42a46332922dddb2d9bd0c4633a9850bd5b9e
Farhan Tariq <farhan@iskaan.com>
2025-04-30 11:05:47 +0500

    merged with branch vms/design

--- lib/view/widgets/activity log/activity_log_widget.dart
+++ lib/view/widgets/activity log/activity_log_widget.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gap/gap.dart';
-import 'package:timeline_tile/timeline_tile.dart'
-    show IndicatorStyle, LineStyle, TimelineAlign, TimelineTile;
+import 'package:timeline_tile/timeline_tile.dart';
 import 'package:visitors/resource/constants/app_colors.dart';
 import 'package:visitors/resource/constants/images.dart';
 import 'package:visitors/resource/styles/styles.dart';
 import 'package:visitors/view/widgets/container_widgets/icon_text_container_widget.dart';
+
+import '../../../resource/constants/app_constants.dart';
 
 class ActivityLogWidget extends StatelessWidget {
   final String? status;
@@ -16,21 +17,21 @@ class ActivityLogWidget extends StatelessWidget {
   final bool? isLast;
   final double? verticalPadding;
   final double? horizontalPadding;
-  const ActivityLogWidget({
-    super.key,
-    this.status,
-    this.byValue,
-    this.description,
-    this.dateTime,
-    this.isLast,
-    this.verticalPadding,
-    this.horizontalPadding
-  });
+  const ActivityLogWidget(
+      {super.key,
+      this.status,
+      this.byValue,
+      this.description,
+      this.dateTime,
+      this.isLast,
+      this.verticalPadding,
+      this.horizontalPadding});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: horizontalPadding ?? 10,vertical: verticalPadding ?? 0),
+      padding: EdgeInsets.symmetric(
+          horizontal: horizontalPadding ?? 10, vertical: verticalPadding ?? 0),
       decoration: BoxDecoration(
         color: AppColors.white,
         borderRadius: BorderRadius.circular(10),
@@ -39,83 +40,85 @@ class ActivityLogWidget extends StatelessWidget {
         children: [
           const Gap(10),
           TimelineTile(
-        alignment: TimelineAlign.values.first,
-        isLast: isLast ?? false,
-        afterLineStyle: const LineStyle(
-          color: AppColors.lightGrey,
-          thickness: 2,
-        ),
-        indicatorStyle: IndicatorStyle(
-          indicatorXY: 0.0,
-          width: 23,
-          height: 23,
-          padding: const EdgeInsets.symmetric(
-            horizontal: 0,
-          ),
-          drawGap: true,
-          indicator:
-          Container(
-            height: AppConstants.isTablet(context) ?30 : 25,
-            width: AppConstants.isTablet(context) ?30 : 25,
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
+            alignment: TimelineAlign.values.first,
+            isLast: isLast ?? false,
+            afterLineStyle: const LineStyle(
+              color: AppColors.lightGrey,
+              thickness: 2,
             ),
-            child:  Center(
-              child: SvgPicture.asset(
-                AppImages.log,
-                height: AppConstants.isTablet(context) ?30 : 25,
-                width: AppConstants.isTablet(context) ?30 : 25,
-                fit: BoxFit.fill,
-                colorFilter: const ColorFilter.mode(
-                  AppColors.primary,
-                  BlendMode.srcIn,
+            indicatorStyle: IndicatorStyle(
+              indicatorXY: 0.0,
+              width: 23,
+              height: 23,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 0,
+              ),
+              drawGap: true,
+              indicator: Container(
+                height: AppConstants.isTablet(context) ? 30 : 25,
+                width: AppConstants.isTablet(context) ? 30 : 25,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
                 ),
-
+                child: Center(
+                  child: SvgPicture.asset(
+                    AppImages.log,
+                    height: AppConstants.isTablet(context) ? 30 : 25,
+                    width: AppConstants.isTablet(context) ? 30 : 25,
+                    fit: BoxFit.fill,
+                    colorFilter: const ColorFilter.mode(
+                      AppColors.primary,
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            endChild: Container(
+              constraints: const BoxConstraints(
+                minHeight: 105,
+                maxWidth: double.infinity,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 6),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(status ?? "",
+                            style: AppConstants.isTablet(context)
+                                ? AppTextStyles.style16black600
+                                : AppTextStyles.style14Black600),
+                        Flexible(
+                          child: Text(byValue ?? "",
+                              style: AppConstants.isTablet(context)
+                                  ? AppTextStyles.style16Primary600
+                                  : AppTextStyles.style14Primary600),
+                        ),
+                      ],
+                    ),
+                    const Gap(5),
+                    Text(
+                      description ?? "",
+                      style: AppConstants.isTablet(context)
+                          ? AppTextStyles.style14DarkGrey600
+                          : AppTextStyles.style12DarkGrey600,
+                    ),
+                    const Gap(10),
+                    IconTextContainerWidget(
+                      image: AppImages.date,
+                      text: dateTime ?? "",
+                    ),
+                    const Gap(20),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        endChild: Container(
-          constraints: const BoxConstraints(
-            minHeight: 105,
-            maxWidth: double.infinity,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.only(left: 6),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Text(status ?? "",
-                        style: AppConstants.isTablet(context) ? AppTextStyles.style16black600 :  AppTextStyles.style14Black600
-                    ),
-                    Flexible(
-                      child: Text(byValue ?? "",
-                          style: AppConstants.isTablet(context) ? AppTextStyles.style16Primary600 :  AppTextStyles.style14Primary600),
-                    ),
-                  ],
-                ),
-                const Gap(5),
-                Text(
-                  description ?? "",
-                  style: AppConstants.isTablet(context) ?  AppTextStyles.style14DarkGrey600 :  AppTextStyles.style12DarkGrey600,
-                ),
-                const Gap(10),
-                IconTextContainerWidget(
-                  image: AppImages.date,
-                  text: dateTime ?? "",
-                ),
-                const Gap(20),
-              ],
-            ),
-          ),
-        ),
-      ),
         ],
       ),
     );
   }
 }
-
