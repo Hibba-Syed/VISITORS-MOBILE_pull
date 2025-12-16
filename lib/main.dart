236fd20d7fb2b06f6d05f0ac0b9924ce970ed00a
Farhan Tariq <farhan@iskaan.com>
2025-12-16 11:46:36 +0500

    removed scanbot sdk

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:scanbot_sdk/scanbot_sdk.dart';
 import 'package:visitors/resource/constants/app_constants.dart';
 import 'package:visitors/utils/preference_utils.dart';
 import 'package:visitors/iskaan_visitors_mobile.dart';
@@ -9,10 +8,6 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
   await initPreferences();
-  ScanbotSdk.initScanbotSdk(ScanbotSdkConfig(
-    licenseKey: AppConstants.trialLicenseKey,
-    loggingEnabled: true,
-  ));
 
   runApp(
     EasyLocalization(
